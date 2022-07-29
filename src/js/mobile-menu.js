5fd2b6dc1898b11ca7b0302a2a264e13611143d9
Kellyson <Kellysondias@outlook.com>
2022-07-29 15:05:17 -0300

    Finishing the 'mobile menu' js code config

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,30 +1,13 @@
-/* const navBar = document.querySelector('.nav-bar') */
-
-
 const mobileMenu = document.querySelector('#hamburger-menu')
 
 mobileMenu.addEventListener('click', () => {
     const backdrop = document.querySelector('.backdrop')
+    const secondBackDropClass = backdrop.classList[1]
 
-    const backdropClasses = backdrop.classList.value
+    console.log(secondBackDropClass)
 
     backdrop.classList.add("show")
-    if (backdropClasses === "show") backdrop.classList.remove("show")
+
+    if (secondBackDropClass === "show") backdrop.classList.remove("show")
+
 })
-
-
-
-
-
-
-
-/* menu.addEventListener('click', () => {
-    if (navBar.getAttribute('class') === 'dropOut') {
-        navBar.classList.remove('dropOut')
-        backdrop.classList.remove("show")
-        
-    } else {
-        navBar.classList.add('dropOut')
-        backdrop.classList.add("show")
-    }
-}) */
